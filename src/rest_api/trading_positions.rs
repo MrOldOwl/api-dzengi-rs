0deee42fc21704a5fe4952cb95b29a474a3ec62a
MrOldOwl <MrOldOwl>
2025-06-08 15:10:59 +0300

    add trading_positions

---
+++ src/rest_api/trading_positions.rs
@@ -0,0 +1,58 @@
+use super::DzengiRestClient;
+use crate::{
+    errors::DzengiRestClientResult,
+    help::{AutoToJson, DefaultKeys, Query},
+    models::TradingPositionListResponse,
+    response_models::RecvWindowRequest,
+    switch_url,
+};
+
+impl DzengiRestClient {
+    pub async fn trading_positions(
+        &self,
+        request: RecvWindowRequest,
+    ) -> DzengiRestClientResult<TradingPositionListResponse> {
+        let settings = self.settings()?;
+
+        let mut query = Query::<2>::new();
+        query.add_item(DefaultKeys::timestamp(&self)?);
+        request.fill_query(&mut query);
+        let signature = query.gen_signature(&settings)?;
+
+        self.client
+            .get(switch_url!("/api/v1/tradingPositions", self.demo))
+            .header(DefaultKeys::api_key(), settings.api_key.as_str())
+            .query(&query.as_slice())
+            .query(&DefaultKeys::signature(&signature))
+            .send_and_json()
+            .await
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use env_file_reader::read_file;
+
+    use crate::{
+        crypto::UserSettings, response_models::RecvWindowRequest, rest_api::DzengiRestClient,
+    };
+
+    #[tokio::test]
+    async fn test() {
+        let ent_file = read_file(".env").unwrap();
+        let api_key = ent_file["API_KEY"].clone();
+        let secret = ent_file["SECRET"].clone();
+
+        let mut rest =
+            DzengiRestClient::new().with_user_settings(Some(UserSettings::new(api_key, secret)));
+
+        rest.calc_correction_with_server().await.unwrap();
+
+        let resp = rest
+            .trading_positions(RecvWindowRequest::new())
+            .await
+            .unwrap();
+
+        println!("{:?}", &resp);
+    }
+}
